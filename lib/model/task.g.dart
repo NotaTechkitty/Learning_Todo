720268aa6fa20b31968d14e5c4e6f62c4be09f31
Thien Le Minh Nhat <thienlmn@rising-stars.vn>
2022-02-12 16:36:48 +0700

    [APP-1] Create model

---
+++ lib/model/task.g.dart
@@ -0,0 +1,111 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task.dart';
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+class _$Task extends Task {
+  @override
+  final int id;
+  @override
+  final String title;
+  @override
+  final bool status;
+
+  factory _$Task([void Function(TaskBuilder)? updates]) =>
+      (new TaskBuilder()..update(updates)).build();
+
+  _$Task._({required this.id, required this.title, required this.status})
+      : super._() {
+    BuiltValueNullFieldError.checkNotNull(id, 'Task', 'id');
+    BuiltValueNullFieldError.checkNotNull(title, 'Task', 'title');
+    BuiltValueNullFieldError.checkNotNull(status, 'Task', 'status');
+  }
+
+  @override
+  Task rebuild(void Function(TaskBuilder) updates) =>
+      (toBuilder()..update(updates)).build();
+
+  @override
+  TaskBuilder toBuilder() => new TaskBuilder()..replace(this);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is Task &&
+        id == other.id &&
+        title == other.title &&
+        status == other.status;
+  }
+
+  @override
+  int get hashCode {
+    return $jf($jc($jc($jc(0, id.hashCode), title.hashCode), status.hashCode));
+  }
+
+  @override
+  String toString() {
+    return (newBuiltValueToStringHelper('Task')
+          ..add('id', id)
+          ..add('title', title)
+          ..add('status', status))
+        .toString();
+  }
+}
+
+class TaskBuilder implements Builder<Task, TaskBuilder> {
+  _$Task? _$v;
+
+  int? _id;
+  int? get id => _$this._id;
+  set id(int? id) => _$this._id = id;
+
+  String? _title;
+  String? get title => _$this._title;
+  set title(String? title) => _$this._title = title;
+
+  bool? _status;
+  bool? get status => _$this._status;
+  set status(bool? status) => _$this._status = status;
+
+  TaskBuilder();
+
+  TaskBuilder get _$this {
+    final $v = _$v;
+    if ($v != null) {
+      _id = $v.id;
+      _title = $v.title;
+      _status = $v.status;
+      _$v = null;
+    }
+    return this;
+  }
+
+  @override
+  void replace(Task other) {
+    ArgumentError.checkNotNull(other, 'other');
+    _$v = other as _$Task;
+  }
+
+  @override
+  void update(void Function(TaskBuilder)? updates) {
+    if (updates != null) updates(this);
+  }
+
+  @override
+  _$Task build() {
+    final _$result = _$v ??
+        new _$Task._(
+            id: BuiltValueNullFieldError.checkNotNull(id, 'Task', 'id'),
+            title:
+                BuiltValueNullFieldError.checkNotNull(title, 'Task', 'title'),
+            status: BuiltValueNullFieldError.checkNotNull(
+                status, 'Task', 'status'));
+    replace(_$result);
+    return _$result;
+  }
+}
+
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
---
+++ lib/store/model/state.g.dart
@@ -0,0 +1,110 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state.dart';
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+class _$AppState extends AppState {
+  @override
+  final String status;
+  @override
+  final BuiltList<dynamic> tasks;
+
+  factory _$AppState([void Function(AppStateBuilder)? updates]) =>
+      (new AppStateBuilder()..update(updates)).build();
+
+  _$AppState._({required this.status, required this.tasks}) : super._() {
+    BuiltValueNullFieldError.checkNotNull(status, 'AppState', 'status');
+    BuiltValueNullFieldError.checkNotNull(tasks, 'AppState', 'tasks');
+  }
+
+  @override
+  AppState rebuild(void Function(AppStateBuilder) updates) =>
+      (toBuilder()..update(updates)).build();
+
+  @override
+  AppStateBuilder toBuilder() => new AppStateBuilder()..replace(this);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is AppState && status == other.status && tasks == other.tasks;
+  }
+
+  @override
+  int get hashCode {
+    return $jf($jc($jc(0, status.hashCode), tasks.hashCode));
+  }
+
+  @override
+  String toString() {
+    return (newBuiltValueToStringHelper('AppState')
+          ..add('status', status)
+          ..add('tasks', tasks))
+        .toString();
+  }
+}
+
+class AppStateBuilder implements Builder<AppState, AppStateBuilder> {
+  _$AppState? _$v;
+
+  String? _status;
+  String? get status => _$this._status;
+  set status(String? status) => _$this._status = status;
+
+  ListBuilder<dynamic>? _tasks;
+  ListBuilder<dynamic> get tasks =>
+      _$this._tasks ??= new ListBuilder<dynamic>();
+  set tasks(ListBuilder<dynamic>? tasks) => _$this._tasks = tasks;
+
+  AppStateBuilder();
+
+  AppStateBuilder get _$this {
+    final $v = _$v;
+    if ($v != null) {
+      _status = $v.status;
+      _tasks = $v.tasks.toBuilder();
+      _$v = null;
+    }
+    return this;
+  }
+
+  @override
+  void replace(AppState other) {
+    ArgumentError.checkNotNull(other, 'other');
+    _$v = other as _$AppState;
+  }
+
+  @override
+  void update(void Function(AppStateBuilder)? updates) {
+    if (updates != null) updates(this);
+  }
+
+  @override
+  _$AppState build() {
+    _$AppState _$result;
+    try {
+      _$result = _$v ??
+          new _$AppState._(
+              status: BuiltValueNullFieldError.checkNotNull(
+                  status, 'AppState', 'status'),
+              tasks: tasks.build());
+    } catch (_) {
+      late String _$failedField;
+      try {
+        _$failedField = 'tasks';
+        tasks.build();
+      } catch (e) {
+        throw new BuiltValueNestedFieldError(
+            'AppState', _$failedField, e.toString());
+      }
+      rethrow;
+    }
+    replace(_$result);
+    return _$result;
+  }
+}
+
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
